3d618843193afe71e2c6404c16e983e38773d428
Liga Daine <liga.daine@daine-it.com>
2021-06-26 16:32:55 +0200

    Catch any errors related to Jimp.

--- image-filter/src/util/util.ts
+++ image-filter/src/util/util.ts
@@ -1,3 +1,4 @@
+import { reject } from 'bluebird';
 import fs from 'fs';
 import Jimp = require('jimp');
 
@@ -11,16 +12,20 @@ const tmpImageFolderRelativePath = "/tmp";
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
-    return new Promise(async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = `${tmpImageFolderRelativePath}/filtered.${Math.floor(Math.random() * 2000)}.jpg`;
-        await photo
-            .resize(256, 256) // resize
-            .quality(60) // set JPEG quality
-            .greyscale() // set greyscale
-            .write(__dirname + outpath, (img) => {
-                resolve(__dirname + outpath);
-            });
+    return new Promise(async (resolve, reject) => {
+        try {
+            const photo = await Jimp.read(inputURL);
+            const outpath = `${tmpImageFolderRelativePath}/filtered.${Math.floor(Math.random() * 2000)}.jpg`;
+            await photo
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .write(__dirname + outpath, (img) => {
+                    resolve(__dirname + outpath);
+                });
+        } catch (error) {
+            reject(error.message);
+        }
     });
 }
 
@@ -34,7 +39,7 @@ export function clearFilterImageFolder(): void {
         if (err) {
             console.error(`Could not read directory: ${dirPath}`);
             return;
-        } 
+        }
         deleteLocalFiles(files.map(fileName => `${dirPath}/${fileName}`))
     });
 }
