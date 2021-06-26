763286e5539a79016c4ccb0e2e645b901ecbae75
Liga Daine <liga.daine@daine-it.com>
2021-06-26 14:02:44 +0200

    Format utils.ts file.

--- image-filter/src/util/util.ts
+++ image-filter/src/util/util.ts
@@ -10,17 +10,17 @@ const tmpImageFolderRelativePath = "/tmp";
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async resolve => {
         const photo = await Jimp.read(inputURL);
         const outpath = `${tmpImageFolderRelativePath}/filtered.${Math.floor(Math.random() * 2000)}.jpg`;
         await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(__dirname + outpath, (img) => {
+                resolve(__dirname + outpath);
+            });
     });
 }
 
@@ -44,8 +44,8 @@ export function clearFilterImageFolder(): void {
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
+function deleteLocalFiles(files: Array<string>) {
+    for (let file of files) {
         fs.unlinkSync(file);
     }
 }
