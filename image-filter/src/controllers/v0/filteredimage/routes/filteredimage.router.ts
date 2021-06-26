f316adef7386fe60bdd7dea5919e93f026436a0d
Liga Daine <liga.daine@daine-it.com>
2021-06-26 15:26:51 +0200

    Check if provided URL is valid.

--- image-filter/src/controllers/v0/filteredimage/routes/filteredimage.router.ts
+++ image-filter/src/controllers/v0/filteredimage/routes/filteredimage.router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { request } from 'http';
 import { filterImageFromURL, clearFilterImageFolder } from '../../../../util/util';
 
 const router: Router = Router();
@@ -8,16 +9,24 @@ router.get('/', async (req: Request, res: Response) => {
     if (!imageUrl) {
         return res.status(400).send({ message: 'Image Url is required.' });
     }
-    try {
-        const filteredImage = await filterImageFromURL(imageUrl);
-        res.sendFile(filteredImage, {}, async (err: any) => {
-            console.log('File is send');
-            clearFilterImageFolder();
-        });
-    } catch (error) {
-        console.error(error);
-        return res.send(500).send(error.message);
-    }
+
+    request({
+        host: imageUrl
+    }, async (response) => {
+        try {
+            const filteredImage = await filterImageFromURL(imageUrl);
+            res.sendFile(filteredImage, {}, async (err: any) => {
+                console.log('File is send.');
+                clearFilterImageFolder();
+            });
+        } catch (error) {
+            console.error(error);
+            return res.send(500).send(error.message);
+        }
+    }).on('error', (e) => {
+        console.error(`Image Url is invalid.`);
+        return res.status(400).send({ message: 'Image Url is invalid.' });
+    }).end();
 });
 
 export const FilteredImageRouter: Router = router;
